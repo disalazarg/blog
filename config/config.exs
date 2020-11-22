63f84b56322380389a9d2fd4cbcd114fdab36fe1
Daniel Salazar <disalazarg@gmail.com>
2020-11-21 21:43:20 -0300

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,35 @@
+# This file is responsible for configuring your umbrella
+# and **all applications** and their dependencies with the
+# help of Mix.Config.
+#
+# Note that all applications in your umbrella share the
+# same configuration and dependencies, which is why they
+# all use the same configuration file. If you want different
+# configurations or dependencies per app, it is best to
+# move said applications out of the umbrella.
+use Mix.Config
+
+
+
+config :blog_web,
+  generators: [context_app: :blog]
+
+# Configures the endpoint
+config :blog_web, BlogWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "MF2mEkMK47zvNcCm+tUV75kFNr4Bl7LQUHbjx48vXgfs7LlgSDQlcK4nFHb0SyYA",
+  render_errors: [view: BlogWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Blog.PubSub,
+  live_view: [signing_salt: "zFyUhyZH"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
